e903fa206ce0b544f1fc371c2d0e00c14366c436
DevFranceca <dynamicfranchescer@gmail.com>
2024-08-08 13:23:52 +0100

    correction

--- src/components/header/page.jsx
+++ src/components/header/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex justify-center items-center bg-black h-20 w-full">
       This is my header
     </div>
   )
