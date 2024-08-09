282080d60e905adca51022c92c80fe12c1841d5b
DevFranceca <dynamicfranchescer@gmail.com>
2024-08-09 13:26:56 +0100

    work on hero component

--- src/components/heroes/page.jsx
+++ src/components/heroes/page.jsx
@@ -4,17 +4,23 @@ const Heroes = () => {
   return (
     <div className="h-screen w-screen bg-[url('/bg2.avif')] bg-cover bg-center bg-no-repeat overflow-hidden flex items-center justify-center"
     >
-      <section className="w-2/3 sm-w-4/5 md-w-3/4 lg-w-2/4 h-auto sm-h-2/3 p-4 sm-p-6 md-p-8 text-center bg-red-400 flex flex-col justify-evenly items-center"
+      <section className="w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/4 h-auto sm:h-2/3 p-4 sm:p-6 md:p-8 text-center bg-red-400 flex flex-col justify-evenly items-center"
       >
         <h1 class="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-teal-600">
           TECH SOLUTIONS
         </h1>
-        <p class="text-xs sm:text-base md:text-md lg:text-lg xl:text-text-base 2xl:text-2xl">
+        <p class="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi similique error asperiores porro modi recusandae laborum deserunt tenetur architecto sed. Aut harum error vero beatae veniam. Tenetur, deserunt! Aut, modi.
           </p>
-        <div className="w-2/3 h-1/4 bg-amber-300 flex justify-center items-center">
-          <button></button>
-          <button></button>
+        <div className="w-full sm:w-1/2 md:w-2/5 h-16 bg-amber-300 flex justify-around items-center"
+        >
+          <button className="bg-teal-500 text-white px-4 py-2 rounded-md hover:bg-teal-600 transition duration-300">
+            Contact Us
+          </button >
+
+          <button className="bg-white text-teal-500 px-4 py-2 rounded-md hover:bg-gray-200 transition duration-300">
+            Get Started
+          </button>
         </div>
       </section>
     </div>
