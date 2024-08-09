803a4a4244dedcea87b769b2d4bc002ed472b8c3
DevFranceca <dynamicfranchescer@gmail.com>
2024-08-09 14:41:20 +0100

    finish hero component

--- src/components/heroes/page.jsx
+++ src/components/heroes/page.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Heroes = () => {
   return (
-    <div className="h-screen w-screen bg-[url('/bg2.avif')] bg-cover bg-center bg-no-repeat overflow-hidden flex items-center justify-center"
-    >
-      <section className="w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/4 h-auto sm:h-2/3 p-4 sm:p-6 md:p-8 text-center bg-red-400 flex flex-col justify-evenly items-center"
+    <div className="h-screen w-screen bg-cover bg-center bg-no-repeat overflow-hidden flex items-center justify-center relative"
+         style={{ backgroundImage: "url('/bg2.avif'), linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2))" }}>
+      <section className="w-11/12 sm:w-4/5 md:w-3/4 lg:w-2/4 h-auto sm:h-2/3 p-4 sm:p-6 md:p-8 text-center flex flex-col justify-evenly items-center  bg-white/30 rounded-lg shadow-md"
       >
         <h1 class="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold text-teal-600">
           TECH SOLUTIONS
         </h1>
-        <p class="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
+        <p class="text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl text-black">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi similique error asperiores porro modi recusandae laborum deserunt tenetur architecto sed. Aut harum error vero beatae veniam. Tenetur, deserunt! Aut, modi.
           </p>
-        <div className="w-full sm:w-1/2 md:w-2/5 h-16 bg-amber-300 flex justify-around items-center"
+        <div className="w-full sm:w-1/2 md:w-2/5 h-16 flex justify-between items-center"
         >
           <button className="bg-teal-500 text-white px-4 py-2 rounded-md hover:bg-teal-600 transition duration-300">
             Contact Us
