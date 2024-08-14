3537c00dd57baa09085d7bd00864bea9dc441f3c
DevFranceca <dynamicfranchescer@gmail.com>
2024-08-14 12:02:46 +0100

    work on service component

--- src/components/services/page.jsx
+++ src/components/services/page.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React from 'react'
 
 const Services = () => {
 
+  const Articles =[
+    {
+      title: "IT and Infrastructure",
+      img: "/IT.jpeg",
+      content: "TechSolution builds and operate most infrastructure. We provide market and industry insights along with expertise that leads to organisational success.",
+    },
+    {
+      title: "Cybersecurity",
+      img: "/cyber.jpeg",
+      content: "TechSolution Cybersecurity service helps you operate securely, grow successfully and secure success for your organisation.",
+    },
+    {
+      title: "Tech Insights",
+      img: "/finance.jpeg",
+      content: "TechSolution helps organisation with their digital transformation which is an essential strategy for groth in our fast paced world today.",
+    },
+  ]
   return (
-    <section>
-      This is service page
-    </section>
-  );
-};
+    <article className="w-screen h-auto bg-red-200 py-8 px-4 sm:px-6 lg:px-8 flex flex-col justify-evenly">
+      <h2 className="text-2xl text-center font-bold mb-6 text-teal-700">Our Services</h2>
+      <section className="w-full flex flex-col sm:flex-row justify-evenly items-center gap-6">
+      {Articles.map((services, index)=>(
+        <div key={index} className="bg-slate-50 shadow-lg rounded-md w-full sm:w-[30%] p-4 flex flex-col items-center hover:scale-90 transform transition duration-300">
+          <h3 className="text-xl text-center font-semibold mb-4">{services.title}</h3>
+          <img src={services.img} alt="image here" className="object-cover w-[90%] sm:w-[80%] h-48 mb-4 rounded-md"/>
+          <span className="text-gray-600">{services.content}</span>
+        </div>
+      ))}
+      </section>
+    </article>
+  )
+}
 
-export default Services;
-
+export default Services
